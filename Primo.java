360cb5bc8397d313dc951cc223d2c7bba480d080
esgesges <74415075+esgesges@users.noreply.github.com>
2022-11-09 12:11:14 +0100

    Add files via upload

---
+++ Primo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.p_ferrian_primo;
+
+
+public class Primo 
+{
+    public static void main(String[] args) 
+    {
+     System.out.println("\n\n\n GUANDALINI E' GAY!!!\n\n\n");
+     int a = 6657;
+     int b = 234;
+     int totale = a + b * (a+a / b);
+     System.out.println(totale + "\n\n" + "\n\n");
+     
+    }
+}

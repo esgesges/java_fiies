b4d5f741ca6f3004079328f34f11a3b079dbb312
esgesges <74415075+esgesges@users.noreply.github.com>
2022-11-09 12:56:59 +0100

    Add files via upload

--- Primo.java
+++ Primo.java
@@ -9,11 +9,45 @@ public class Primo
 {
     public static void main(String[] args) 
     {
-     System.out.println("\n\n\n GUANDALINI E' GAY!!!\n\n\n");
-     int a = 6657;
-     int b = 234;
-     int totale = a + b * (a+a / b);
-     System.out.println(totale + "\n\n" + "\n\n");
-     
+        
+      int a = 5;
+      int b = 10;
+      int somma = a + b;
+      int sottrazione = a - b;
+      int moltiplicazione = a * b;
+      int divisione = a / b;
+       
+      if ( somma <= 100)
+      {
+          System.out.println(" la somma di a e b è:" + somma + " ed è <= di 100");
+      }
+      else
+      {
+            System.out.println(" la somma di a e b è:" + somma + " e non è <= di 100");
+      }
+      if ( sottrazione <= 100)
+      {
+          System.out.println(" la sottrazione di a e b è:" + sottrazione + " ed è <= di 100");
+      }
+      else 
+      {
+            System.out.println(" la sottrazione di a e b è:" + sottrazione + " e non è <= di 100");
+      }
+      if ( sottrazione <= 100)
+      {
+          System.out.println(" la molitplicazione di a e b è:" + moltiplicazione + " ed è <= di 100");
+      }
+      else 
+      {
+            System.out.println(" la molitplicazione di a e b è:" + moltiplicazione + " e non è <= di 100");
+      }
+      if ( divisione <= 100)
+      {
+          System.out.println(" la divisione di a e b è:" + divisione + " ed è <= di 100");
+      }
+      else 
+      {
+            System.out.println(" la divisione di a e b è:" + divisione + " e non è <= di 100");
+      }
     }
 }
